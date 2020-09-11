2b46b340d087a5fd052530153f283ad17a49c1ab
Anshul Jain <anshul.jain@elogist.in>
2020-09-11 12:44:48 +0530

    frontend added

--- routes/getnews.js
+++ routes/getnews.js
@@ -7,23 +7,48 @@ const newsapi = new NewsAPI('6ff0398454074a969183758dbd1b62cc');
 
 router.get('/top-stories', (req, res) => {
     let data;
-    newsapi.v2.topHeadlines({
-        sources: 'bbc-news,the-verge',
-        // q: 'bitcoin',
-        category: 'business',
-        language: 'en',
-        country: 'us'
-      }).then(response => {
+    let param = {
+      sources: 'bbc-news,the-verge',
+      // q: 'bitcoin',
+      category: 'business',
+      language: 'en',
+      country: 'us'
+    }
+    newsapi.v2.topHeadlines(param).then(response => {
         console.log(response);
         data = response;
         res.status(200).json(response);
       });
 });
 
-router.get('/news/:term', (req, res) => {
+// router.get('/news/', (req, res) => {
+ 
+//   newsapi.v2.sources({
+//     category: 'technology',
+//     language: 'en',
+//     country: 'us'
+//   }).then(response => {
+//     console.log(response);
+//   });
+// });
 
+router.get('/news/', (req, res) => {
+  let param = req.query;
+  console.log(param);
+  let data = {
+    q: param.term,
+    sources: 'bbc-news,the-verge',
+    domains: 'bbc.co.uk,techcrunch.com',
+    // from: '2017-12-01',
+    // to: '2017-12-12',
+    language: 'en',
+    sortBy: 'relevancy',
+    page: 2
+  }
+  newsapi.v2.everything().then(response => {
+    
+    res.status(200).json(response);
+  });
 });
 
-router.get('/news', (req, res) => {
-
-});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const getNews = require('./routes/getnews');
 const app = express();
 
 const port = process.env.Port || 3000; 
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/newsfeedly', { useNewUrlParser: true
         console.log(err);
     })
 
+app.use(getNews);
 app.get('/', (req, res) => {
     res.send('hello');
 });
